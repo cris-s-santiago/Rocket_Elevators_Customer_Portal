7227b4481f2cfa6c23ac340c5cdaaca5ee99a09f
Cristiane Santiago <cris.s.santiago@gmail.com>
2021-04-08 09:56:16 -0400

    First API call added, registration only allowed for customers

--- Models/Building.cs
+++ Models/Building.cs
@@ -21,20 +21,4 @@ namespace Rocket_Elevators_Customer_Portal.Models
         public List<Battery> batteries { get; set; }
         public Customer customer { get; set; }
     }
-
-
-// BUILDING TABLE from the schema of the Ruby on Rails's base project
-/* create_table "buildings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do | t |
-    t.string "full_name_of_the_building_administrator"
-    t.string "email_of_the_administrator_of_the_building"
-    t.string "phone_number_of_the_building_administrator"
-    t.string "full_name_of_the_technical_contact_for_the_building"
-    t.string "technical_contact_email_for_the_building"
-    t.string "technical_contact_phone_for_the_building"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "customer_id"
-    t.bigint "address_id"
-    t.index["address_id"], name: "index_buildings_on_address_id"
-    t.index["customer_id"], name: "index_buildings_on_customer_id"
-  end */
+}
